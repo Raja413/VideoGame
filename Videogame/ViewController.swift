a8d5805334a1732151d23489cc6e81a570ddbdf0
Raja <monicachoicarter@gmail.com>
2018-10-10 16:41:46 -0700

    Initial Commit

---
+++ Videogame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Videogame
+//
+//  Created by Raja reddy Poreddy on 10/10/18.
+//  Copyright © 2018 Delvelogic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
